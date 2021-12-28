5e046cfc93751c2167396d25c718bf5876c3f346
Jorge Rinaldi <jrg.rinaldi@gmail.com>
2021-12-28 11:59:07 +0100

    DerefMut implemented

--- src/deref.rs
+++ src/deref.rs
@@ -1,5 +1,5 @@
 use crate::LocalVec;
-use std::ops::Deref;
+use std::ops::{Deref, DerefMut};
 
 impl<T, const N: usize> Deref for LocalVec<T, N> {
     type Target = [T];
@@ -8,6 +8,14 @@ impl<T, const N: usize> Deref for LocalVec<T, N> {
         unsafe {
            std::slice::from_raw_parts(self.as_ptr(), self.len)
         }
+    }
+}
+
+impl<T, const N: usize> DerefMut for LocalVec<T, N> {
+    fn deref_mut(&mut self) -> &mut [T] {
+        unsafe {
+            std::slice::from_raw_parts_mut(self.as_mut_ptr(), self.len)
+         }
     }
 }
 
@@ -25,9 +33,25 @@ mod tests {
     }
 
     #[test]
+    fn test_deref_mut() {
+        let arr = [33; 3];
+        let mut vec = LocalVec::<_, 8>::from_array(arr);
+        let slc = vec.deref_mut();
+        assert_eq!(slc.len(), 3);
+        assert_eq!(slc, arr);
+    }
+
+    #[test]
     fn test_deref_zero_size() {
         let vec = LocalVec::<u8, 0>::new();
         let slc = vec.deref();
+        assert_eq!(slc.len(), 0);
+    }
+
+    #[test]
+    fn test_deref_mut_zero_size() {
+        let mut vec = LocalVec::<u8, 0>::new();
+        let slc = vec.deref_mut();
         assert_eq!(slc.len(), 0);
     }
 }
