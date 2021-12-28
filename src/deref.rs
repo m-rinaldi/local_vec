30bf80c178dcc5e947b1346a99413f3c215d293c
Jorge Rinaldi <jrg.rinaldi@gmail.com>
2021-12-28 11:48:07 +0100

    Deref implemented

--- src/deref.rs
+++ src/deref.rs
@@ -1,9 +1,33 @@
 use crate::LocalVec;
+use std::ops::Deref;
 
-impl<T, const N: usize> std::ops::Deref for LocalVec<T, N> {
+impl<T, const N: usize> Deref for LocalVec<T, N> {
     type Target = [T];
 
-    fn deref(&self) -> &Self::Target {
-        todo!()
+    fn deref(&self) -> &[T] {
+        unsafe {
+           std::slice::from_raw_parts(self.as_ptr(), self.len)
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_deref() {
+        let arr = [33; 3];
+        let vec = LocalVec::<_, 8>::from_array(arr);
+        let slc = vec.deref();
+        assert_eq!(slc.len(), 3);
+        assert_eq!(slc, arr);
+    }
+
+    #[test]
+    fn test_deref_zero_size() {
+        let vec = LocalVec::<u8, 0>::new();
+        let slc = vec.deref();
+        assert_eq!(slc.len(), 0);
     }
 }
