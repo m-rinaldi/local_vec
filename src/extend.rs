5af9aad90c22927f5d4b31f9dc05c64fa82c198b
Jorge Rinaldi <jrg.rinaldi@gmail.com>
2021-12-29 16:34:20 +0100

    tests for Extend

--- src/extend.rs
+++ src/extend.rs
@@ -8,3 +8,30 @@ impl<T, const N: usize> Extend<T> for LocalVec<T, N> {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_extend() {
+        let mut vec = LocalVec::<_, 4>::new();
+        let arr = [1, 2, 3, 4];
+        vec.extend(arr);
+
+        matches!(vec.pop(), Some(4));
+        matches!(vec.pop(), Some(3));
+        matches!(vec.pop(), Some(2));
+        matches!(vec.pop(), Some(1));
+        matches!(vec.pop(), None);
+    }
+
+
+    #[test]
+    #[should_panic]
+    fn test_extend_exceeding() {
+        let mut vec = LocalVec::<_, 3>::new();
+        let arr = [1, 2, 3, 4];
+        vec.extend(arr);
+    }
+}
